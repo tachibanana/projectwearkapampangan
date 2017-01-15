87cb9e756452e285dc0899bef78a0c40d08c6e51
rafaelmanuel <rafaelmanuel00@gmail.com>
2017-01-15 14:56:48 +0800

    add login service

--- src/com/app/wearkapampangan/action/LoginAction.java
+++ src/com/app/wearkapampangan/action/LoginAction.java
@@ -1,23 +1,43 @@
 package com.app.wearkapampangan.action;
 
-public class LoginAction {
+import com.app.wearkapampangan.model.User;
+import com.app.wearkapampangan.service.LoginService;
+import com.opensymphony.xwork2.ActionSupport;
+import com.opensymphony.xwork2.ModelDriven;
+
+import freemarker.template.utility.StringUtil;
+
+public class LoginAction extends ActionSupport implements ModelDriven<User> {
 	
-	private String name;
+	private static final long serialVersionUID = 1L;
+	private User user = new User();
+
+	public User getUser() {
+		return user;
+	}
+
+	public void setUser(User user) {
+		this.user = user;
+	}
 	
+	@Override
+	public void validate(){
+		if(StringUtil.emptyToNull(user.getUsername()) == null)
+			this.addFieldError("username", "Cant have a blank username");
+		if(StringUtil.emptyToNull(user.getPassword()) == null)
+			this.addFieldError("password", "Cant have a blank password");
+	}
+
+	@Override
+	public User getModel() {
+		return user;
+	}
 	
+	@Override
 	public String execute(){
-		System.out.println(name);
-		return "login";
+		if(LoginService.isValidate(user))
+			return SUCCESS;
+		else
+			return ERROR;
 	}
-
-
-	public String getName() {
-		return name;
-	}
-
-
-	public void setName(String name) {
-		this.name = name;
-	}
-
 }
--- src/com/app/wearkapampangan/service/LoginService.java
+++ src/com/app/wearkapampangan/service/LoginService.java
@@ -1,7 +1,17 @@
 package com.app.wearkapampangan.service;
 
-public class LoginService {
-	
-	
+import com.app.wearkapampangan.model.User;
 
+public class LoginService {
+	public static boolean isValidate(User user){
+		try{
+			if(user.getUsername().equals("rafael") && user.getPassword().equals("admin")){
+				return true;
+			}
+			return false;
+		}catch(NullPointerException e){
+			e.printStackTrace();
+			return false;
+		}
+	}
 }
